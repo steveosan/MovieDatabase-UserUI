936055f9e24a638bd085a82ad7439ac947725d97
steveosan <70219345+steveosan@users.noreply.github.com>
2021-12-09 17:51:53 -0600

    Create MovieContext.cs

---
+++ Context/MovieContext.cs
@@ -0,0 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using MovieLibraryOO.DataModels;
+
+namespace MovieLibraryOO.Context
+{
+    public class MovieContext : DbContext
+    {
+        public DbSet<Genre> Genres {get;set;}
+        public DbSet<Movie> Movies {get;set;}
+        public DbSet<MovieGenre> MovieGenres {get;set;}
+        public DbSet<Occupation> Occupations {get;set;}
+        public DbSet<User> Users {get;set;}
+        public DbSet<UserMovie> UserMovies {get;set;}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+                // optionsBuilder.UseLazyLoadingProxies().UseSqlServer(@"Server=bitsql.wctc.edu;Database=mmcarthey_22097_Movie;User ID=mmcarthey;Password=000075813;");
+                optionsBuilder.UseSqlServer(@"Server=bitsql.wctc.edu;Database=MovieDB_12090_sosantowski;User ID=sosantowski1;Password=000479994;");
+        }
+    }
+}
